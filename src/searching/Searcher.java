43e149b3a024bc6a74eeeb6f18fa107795b3ec18
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2011-05-04 23:53:10 +0300

    Adding main class too

---
+++ src/searching/Searcher.java
@@ -0,0 +1,14 @@
+package searching;
+
+import searching.gui.SearcherGUI;
+
+public class Searcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new SearcherGUI();
+	}
+
+}
